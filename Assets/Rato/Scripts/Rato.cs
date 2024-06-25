a1d252819f67983fb9df09ea7b66e58d1d022603
joana4321 <161602331+joana4321@users.noreply.github.com>
2024-06-25 13:36:08 -0300

    25/06

--- Assets/Rato/Scripts/Rato.cs
+++ Assets/Rato/Scripts/Rato.cs
@@ -87,7 +87,7 @@ public class Rato : MonoBehaviour
     }
 
     void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.layer == 6 || collision.gameObject.tag == "Gato"){
+        if(collision.gameObject.layer == 6 || collision.gameObject.layer == 9 ||collision.gameObject.tag == "Gato"){
             isjumping = false;
             animator.SetBool("jump", false);
         }

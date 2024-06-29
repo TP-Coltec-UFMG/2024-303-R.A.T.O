9c9432de61cea0d46e04d627ca5f2c097e479de0
Bruna Santarelli <brunammsantarelli@gmail.com>
2024-06-29 00:49:08 -0300

    dialogo100

--- Assets/Rato/Scripts/DialogueManager.cs
+++ Assets/Rato/Scripts/DialogueManager.cs
@@ -69,7 +69,7 @@ public class DialogueManager : MonoBehaviour
             foreach (DialogueResponse response in currentDialogueNodes[currentIndex].responses){
                 GameObject buttonObj;
                 if (i == 0){
-                    buttonObj = Instantiate(responseButtonPrefab, new Vector3(60, 30, 0), Quaternion.identity, responseButtonContainer);
+                    buttonObj = Instantiate(responseButtonPrefab, new Vector3(100, 30, 0), Quaternion.identity, responseButtonContainer);
                 }else if (i == 1){
                     buttonObj = Instantiate(responseButtonPrefab, new Vector3(300, 30, 0), Quaternion.identity, responseButtonContainer);
                 } else {

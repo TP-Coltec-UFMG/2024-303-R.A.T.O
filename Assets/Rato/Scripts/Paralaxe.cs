3295130f8e9a0590b56c025b185cfaa169739a71
joana4321 <161602331+joana4321@users.noreply.github.com>
2024-08-16 13:34:57 -0300

    resolvi de um jeito ruim o problema paralaxico

--- Assets/Rato/Scripts/Paralaxe.cs
+++ Assets/Rato/Scripts/Paralaxe.cs
@@ -1,53 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 
 public class Paralaxe : MonoBehaviour
 {
-    private float comprimento;
-    private float Posinicio;
-    [SerializeField] private GameObject cam;
-    [SerializeField] private float EfeitoParalaxe;
+    [SerializeField] private float parallaxSpeed = 0.5f;
+    private float startPosition;
+    private float length;
+    private Transform cam;
 
-    // Start is called before the first frame update
     void Start()
     {
-        Posinicio = transform.position.x; //pega o x do objeto
-        comprimento = gameObject.GetComponent<SpriteRenderer>().bounds.size.x;
+        cam = Camera.main.transform;
+        startPosition = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float distancia = (cam.transform.position.x *(1-EfeitoParalaxe));//posição da camera multiplicada pelo efeito
-        float lugar = (cam.transform.position.x *EfeitoParalaxe);
-        transform.position = new Vector3(Posinicio+ distancia, transform.position.y, transform.position.z);
+        float temp = (cam.position.x * (1 - parallaxSpeed));
+        float distance = (cam.position.x * parallaxSpeed);
 
-        /*if(lugar>Posinicio+comprimento){
-            Posinicio+=comprimento;
-        }
-        else if(lugar>Posinicio-comprimento){
-            Posinicio-=comprimento;
-        }*/
+        transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+
+        if (temp > startPosition + length) startPosition += length;
+        else if (temp < startPosition - length) startPosition -= length;
     }
 }
-
-/*public class Paralaxe : MonoBehaviour {
-    [SerializeField] private float velocidade;
-    private Vector3 posicaoInicial;
-    private float tamanhoRealDaImagem ;
-    private void Awake(){
-        this.posicaoInicial = this.transform.position;
-        float tamanhoDaImagem =
-
-        this.GetComponent<SpriteRenderer>().size.x;
-        float escala = this.transform.localScale.x;
-        this.tamanhoRealDaImagem = tamanhoDaImagem * escala;
-    }
-
-    void Update() {
-        float deslocamento = Mathf.Repeat(this.velocidade * Time.time, this.tamanhoRealDaImagem);
-        this.transform.position = this.posicaoInicial + Vector3.left * deslocamento;
-    }
-}*/

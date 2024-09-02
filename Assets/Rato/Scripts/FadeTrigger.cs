76dc09d5f2d7412993a747148637891712788b6a
Marco Túlio Oliveira <marcotuliooliveira216@gmail.com>
2024-09-02 13:04:33 -0300

    mais cidade

---
+++ Assets/Rato/Scripts/FadeTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject Change;
+    [SerializeField] private float FinalOpacity;
+
+    void OnTriggerEnter2D(Collider2D collider){
+        if(collider.gameObject.tag == "Player"){
+            StartCoroutine(Fade());
+        }
+    }
+
+    private IEnumerator Fade(){
+        SpriteRenderer image = this.Change.gameObject.GetComponent<SpriteRenderer>();
+        Color color = image.color;
+        float currentOpacity = 0;
+
+        while(image.color.a != FinalOpacity){
+            image.color = new Color(color.r, color.g, color.b, currentOpacity);
+            yield return new WaitForSeconds(0.05f);
+            currentOpacity += 0.01f;   
+        }
+    }
+}
